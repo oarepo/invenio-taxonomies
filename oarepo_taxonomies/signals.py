8366301a8323f73b893a6a2d4c71b5016675de07
Daniel Kopecký <kopecky.d@gmail.com>
2020-09-01 09:09:45 +0200

    Added signals

--- oarepo_taxonomies/signals.py
+++ oarepo_taxonomies/signals.py
@@ -1,8 +1,31 @@
+import logging
+
 from oarepo_references.proxies import current_references
+
+from oarepo_taxonomies.exceptions import DeleteAbortedError
+
+logger = logging.getLogger(__name__)
+
+formatter = logging.Formatter('%(asctime)s | %(name)s |  %(levelname)s: %(message)s')
+logger.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(stream_handler)
 
 
 def taxonomy_delete(*args, **kwargs):
-    print("taxonomy_delete", args, kwargs)
+    tax = kwargs["taxonomy"]
+    url = tax.links().envelope["self"]
+    raise_aborted_error(url)
+
+
+def taxonomy_term_delete(*args, **kwargs):
+    term = kwargs["term"]
+    url = term.links().envelope["self"]
+    raise_aborted_error(url)
 
 
 def taxonomy_term_update(*args, **kwargs):
@@ -10,12 +33,23 @@ def taxonomy_term_update(*args, **kwargs):
 
 
 def taxonomy_term_moved(*args, **kwargs):
-    print("taxonomy_term_update", args, kwargs)
+    old_term = kwargs["term"]
+    new_term = kwargs["new_term"]
+    old_url = old_term.links().envelope["self"]
+    new_url = new_term.links().envelope["self"]
+    changed_records = current_references.reference_changed(old=old_url, new=new_url)
+    logger.debug(f"Changed records: {changed_records}")
 
 
-def taxonomy_term_created(*args, **kwargs):
-    print("taxonomy_term_update", args, kwargs)
+def raise_aborted_error(url):
+    record_ids, records = get_records(url)
+    if records:
+        raise DeleteAbortedError(
+            f"You cannot delete a taxonomy that is contained in references. First, delete the "
+            f"references in these records: {record_ids}")
 
 
-def taxonomy_term_delete(*args, **kwargs):
-    print("taxonomy_term_delete", args, kwargs)
+def get_records(url):
+    records = current_references.get_records(url)
+    record_ids = [rec.record_id for rec in records]
+    return record_ids, records
--- tests/test_signals.py
+++ tests/test_signals.py
@@ -1,5 +1,92 @@
-from flask_taxonomies.signals import before_taxonomy_deleted
+import pytest
+from flask_taxonomies.proxies import current_flask_taxonomies
+from flask_taxonomies.term_identification import TermIdentification
+from invenio_records import Record
+from oarepo_references.proxies import current_references
+
+from oarepo_taxonomies.exceptions import DeleteAbortedError
 
 
-def test_taxonomy_delete(app):
-    before_taxonomy_deleted.send("ahoj")
+def test_taxonomy_delete(app, db, taxonomy_tree, test_record):
+    taxonomies = current_flask_taxonomies.list_taxonomies(session=None).all()
+    assert len(taxonomies) == 1
+    with pytest.raises(DeleteAbortedError):
+        current_flask_taxonomies.delete_taxonomy(taxonomies[0])
+    taxonomies = current_flask_taxonomies.list_taxonomies(session=None).all()
+    assert len(taxonomies) == 1
+
+
+def test_taxonomy_delete_2(app, db, taxonomy_tree):
+    taxonomies = current_flask_taxonomies.list_taxonomies(session=None).all()
+    assert len(taxonomies) == 1
+    current_flask_taxonomies.delete_taxonomy(taxonomies[0])
+    taxonomies = current_flask_taxonomies.list_taxonomies(session=None).all()
+    assert len(taxonomies) == 0
+
+
+def test_taxonomy_term_delete(app, db, taxonomy_tree):
+    taxonomy = current_flask_taxonomies.get_taxonomy("test_taxonomy")
+    terms = current_flask_taxonomies.list_taxonomy(taxonomy).all()
+    term = terms[1]
+    ti = TermIdentification(term=term)
+    current_flask_taxonomies.delete_term(ti)
+
+
+def test_taxonomy_term_delete_2(app, db, taxonomy_tree, test_record):
+    taxonomy = current_flask_taxonomies.get_taxonomy("test_taxonomy")
+    terms = current_flask_taxonomies.list_taxonomy(taxonomy).all()
+    term = terms[1]
+    ti = TermIdentification(term=term)
+    with pytest.raises(DeleteAbortedError):
+        current_flask_taxonomies.delete_term(ti)
+
+
+def test_taxonomy_term_moved(app, db, taxonomy_tree, test_record):
+    taxonomy = current_flask_taxonomies.get_taxonomy("test_taxonomy")
+    terms = current_flask_taxonomies.list_taxonomy(taxonomy).all()
+    old_record = Record.get_record(id_=test_record.id)
+    old_taxonomy = old_record["taxonomy"]
+    assert old_taxonomy == [{
+        'is_ancestor': True,
+        'links': {
+            'self': 'http://127.0.0.1:5000/2.0/taxonomies/test_taxonomy/a'
+        },
+        'test': 'extra_data'
+    },
+        {
+            'is_ancestor': True,
+            'links': {
+                'parent':
+                    'http://127.0.0.1:5000/2.0/taxonomies/test_taxonomy/a',
+                'self': 'http://127.0.0.1:5000/2.0/taxonomies/test_taxonomy/a/b'
+            },
+            'test': 'extra_data'
+        },
+        {
+            'is_ancestor': False,
+            'links': {
+                'parent': 'http://127.0.0.1:5000/2.0/taxonomies/test_taxonomy/a/b',
+                'self': 'http://127.0.0.1:5000/2.0/taxonomies/test_taxonomy/a/b/c'
+            },
+            'test': 'extra_data'
+        }]
+    ti = TermIdentification(term=terms[2])
+    current_flask_taxonomies.move_term(ti, new_parent=terms[0], remove_after_delete=False)
+    db.session.commit()
+    new_record = Record.get_record(id_=test_record.id)
+    new_taxonomy = new_record["taxonomy"]
+    assert new_taxonomy == [{
+        'is_ancestor': True,
+        'links': {
+            'self': 'http://127.0.0.1:5000/2.0/taxonomies/test_taxonomy/a'
+        },
+        'test': 'extra_data'
+    },
+        {
+            'is_ancestor': False,
+            'links': {
+                'parent': 'http://127.0.0.1:5000/2.0/taxonomies/test_taxonomy/a',
+                'self': 'http://127.0.0.1:5000/2.0/taxonomies/test_taxonomy/a/c'
+            },
+            'test': 'extra_data'
+        }]
