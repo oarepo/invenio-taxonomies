054a52db9227c5e3f25bfe4cb77008e4dbd4518b
Daniel Kopecký <kopecky.d@gmail.com>
2020-09-03 09:44:43 +0200

    Fixed oarepo dependecy

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ readme = open('README.md').read()
 tests_require = [
     'pytest',
     'pytest-cov',
-    'oarepo[deploy-es7]'
+    'oarepo[tests]'
 ]
 extras_require = {
     "tests": tests_require
